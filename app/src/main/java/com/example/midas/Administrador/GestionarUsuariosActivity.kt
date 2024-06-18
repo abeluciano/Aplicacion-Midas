039cb61926e435bddfc081aa16f95be055da82eb
abeluciano <aaragona@ulasalle.edu.pe>
2024-06-18 01:37:53 -0500

    me olvide de un boton de retroceder, ya lo corregimos

--- app/src/main/java/com/example/midas/Administrador/GestionarUsuariosActivity.kt
+++ app/src/main/java/com/example/midas/Administrador/GestionarUsuariosActivity.kt
@@ -1,6 +1,7 @@
 package com.example.midas.Administrador
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +12,11 @@ class GestionarUsuariosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gestionar_usuarios)
+
+        val btnAtrasUsers = findViewById<ImageButton>(R.id.btnAtrasUsers)
+
+        btnAtrasUsers.setOnClickListener(){
+            finish()
+        }
     }
 }
