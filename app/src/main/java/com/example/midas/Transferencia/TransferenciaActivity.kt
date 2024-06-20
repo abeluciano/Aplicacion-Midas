606a92b9f32cfe46a3db079fd5e482c56a7e42c0
abeluciano <aaragona@ulasalle.edu.pe>
2024-06-20 14:47:14 -0500

    actualizando la tabla de reportes, agregando una respuesta, tambien enlazando a las pantallas  de transferencia

--- app/src/main/java/com/example/midas/Transferencia/TransferenciaActivity.kt
+++ app/src/main/java/com/example/midas/Transferencia/TransferenciaActivity.kt
@@ -38,7 +38,7 @@ class TransferenciaActivity : AppCompatActivity() {
         idCuenta = intent.getStringExtra("ID_CUENTA") ?: ""
         tipoMoneda = intent.getStringExtra("TIPO_MONEDA") ?: ""
 
-        idCuentaTextView.text = "ID Cuenta: $idCuenta"
+        idCuentaTextView.text = "$idCuenta"
         simboloMonedaTextView.text = if (tipoMoneda == "Soles") "S/" else "$"
 
         btnAtras.setOnClickListener() {
