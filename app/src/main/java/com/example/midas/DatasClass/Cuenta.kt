5c765422218142c483a52644ede174e38fbd88d4
Rodrigo270103 <91211085+Rodrigo270103@users.noreply.github.com>
2024-06-20 19:49:04 -0500

    documentacion de la data class Cuenta

--- app/src/main/java/com/example/midas/DatasClass/Cuenta.kt
+++ app/src/main/java/com/example/midas/DatasClass/Cuenta.kt
@@ -1,3 +1,16 @@
+/**
+ * Clase que representa una cuenta en la aplicación Midas.
+ *
+ * Autores:
+ * Abel Luciano Aragón Alvaro
+ * Josshua David Flores Chumbimuni
+ * Rodrigo Ojeda Arce
+ *
+ * Resumen:
+ * La clase Cuenta es una data class que almacena la información básica de una cuenta,
+ * incluyendo el ID de la cuenta, el saldo y el tipo de moneda.
+ */
+
 package com.example.midas.DatasClass
 
 data class Cuenta(var idCuenta: String, val saldo: Double, val tipoMoneda: String)
