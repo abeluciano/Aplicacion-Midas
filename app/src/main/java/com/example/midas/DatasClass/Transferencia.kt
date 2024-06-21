d5bd05188b2a353409c5531bfbaab8651f5ebbbe
Rodrigo270103 <91211085+Rodrigo270103@users.noreply.github.com>
2024-06-20 19:56:02 -0500

    documentacion de la data class Transferencia

--- app/src/main/java/com/example/midas/DatasClass/Transferencia.kt
+++ app/src/main/java/com/example/midas/DatasClass/Transferencia.kt
@@ -1,3 +1,17 @@
+/**
+ * Clase que representa una transferencia de dinero en la aplicación Midas.
+ *
+ * Autores:
+ * Abel Luciano Aragón Alvaro
+ * Josshua David Flores Chumbimuni
+ * Rodrigo Ojeda Arce
+ *
+ * Resumen:
+ * La clase Transferencia es una data class que almacena la información de una transferencia
+ * realizada entre cuentas, incluyendo el monto transferido, la fecha, las cuentas de origen y destino,
+ * los nombres asociados a esas cuentas (si están disponibles) y el tipo de transferencia ("Salida" o "Entrada").
+ */
+
 package com.example.midas.DatasClass
 
 import java.util.Date
