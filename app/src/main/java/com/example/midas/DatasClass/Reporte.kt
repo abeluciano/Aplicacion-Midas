57594a260c644efdb1828e6b859aa81f7fbc4afa
Rodrigo270103 <91211085+Rodrigo270103@users.noreply.github.com>
2024-06-20 19:53:14 -0500

    documentacion de la data class de Reportes

--- app/src/main/java/com/example/midas/DatasClass/Reporte.kt
+++ app/src/main/java/com/example/midas/DatasClass/Reporte.kt
@@ -1,3 +1,16 @@
+/**
+ * Clase que representa un reporte en la aplicación Midas.
+ *
+ * Autores:
+ * Abel Luciano Aragón Alvaro
+ * Josshua David Flores Chumbimuni
+ * Rodrigo Ojeda Arce
+ *
+ * Resumen:
+ * La clase Reporte es una data class que almacena la información básica de un reporte,
+ * incluyendo el tipo de reporte, su estado y la fecha y hora en que fue creado.
+ */
+
 package com.example.midas.DatasClass
 
 data class Reporte(var tipoReporte: String, val estado: String, val FechayHora: String)
